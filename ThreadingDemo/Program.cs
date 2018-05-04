3f5bf417cb97db521496eda8ed6df88d7054e337
Jeremy Wells <jwells@vertafore.com>
2018-05-04 15:34:49 -0500

    demo

--- ThreadingDemo/Program.cs
+++ ThreadingDemo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ThreadingDemo
@@ -10,6 +11,24 @@ namespace ThreadingDemo
     {
         static void Main(string[] args)
         {
+            Thread thread = new Thread(WriteUsingNewThread);
+            thread.Start();
+
+            // the main program and the thread program will run simultaneously, both will be interspersed.
+            // this process is deferred to the operating system, cannot be controlled by developer like this.
+            for (var i = 0; i < 1000; i++)
+            {
+                Console.Write(" A" + i + " ");
+            }
+            
+        }
+
+        private static void WriteUsingNewThread()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                Console.Write(" Z" + i + " ");
+            }
         }
     }
 }
