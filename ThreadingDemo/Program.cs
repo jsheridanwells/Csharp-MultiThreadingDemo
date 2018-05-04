e9eef1a174ba1ba18127ac1f03f7e7a2389553df
Jeremy Wells <jwells@vertafore.com>
2018-05-04 15:38:57 -0500

    named the threads

--- ThreadingDemo/Program.cs
+++ ThreadingDemo/Program.cs
@@ -12,10 +12,12 @@ namespace ThreadingDemo
         static void Main(string[] args)
         {
             Thread thread = new Thread(WriteUsingNewThread);
+            thread.Name = "worker thread";
             thread.Start();
 
             // the main program and the thread program will run simultaneously, both will be interspersed.
             // this process is deferred to the operating system, cannot be controlled by developer like this.
+            Thread.CurrentThread.Name = "main thread";
             for (var i = 0; i < 1000; i++)
             {
                 Console.Write(" A" + i + " ");
